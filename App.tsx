2d25d41d85ffbf61a958f4af1a3c35e4a4e5ff2f
Francisco D <drz.y2k@gmail.com>
2023-01-17 17:54:21 -0600

    cambios

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
 import { useFetchCharacters } from './hooks/characters';
 import CharacterCard from './components/CharacterCard';
+import { ModalConfirm } from './ui/ModalConfirm';
+import { createPortal } from 'react-dom';
+import CharactersGrid from './components/CharactersGrid';
 
 export default function App() {
-  const { characters } = useFetchCharacters();
-
-  return (
-    <React.Fragment>
-      <div className="container-fluid mt-3">
-        <div className="row">
-          {characters.map((character) => (
-            <div key={character.id} className="col-md-3 mb-3">
-              <CharacterCard character={character} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </React.Fragment>
-  );
+  return <CharactersGrid />;
 }
--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import App from './App';
 
